58f115fe41644b9b82181707051568d2eff04617
simotn05 <simo.tantaoui.ana@gmail.com>
2023-09-09 14:04:12 +0100

    mod

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Auto-Ecole</title>
+   <link rel="stylesheet" href="styleheader.css">
+</head>
+
+<body>
+    <header class="flexible-header">
+        <!-- <a href="index.php" class="logo"> <img src="images/logo4.png" alt=""></a> -->
+        <a href="index.php" class="logo"> Auto-Ecole</a>
+        <nav class="navigation">
+          <ul> 
+            <li><a href="index.php">Acceuil</a></li>
+            <li><a href="formation.php">Formules</a></li>
+            <li><a href="infos.php">Infos</a></li>
+            <li><a  href="contact.php">Contact</a></li>
+            <li><a id="inscription" href="inscription.php">S'inscrire</a></li>
+          </ul>   
+        </nav>
+    </header>
+    
+  <div class="container">
+    <div id="nav1"></div>
+    <div id="nav2"></div>
+
+      
+      <div id="bienvenue">
+          <div id="img1"> 
+          <div class="animation-container">
+             <div class="image-slider">
+            <img src="images/acceuil0.jpeg" alt="Image 1" class="slider-image">
+            <img src="images/acceuil2.jpeg" alt="Image 2" class="slider-image">
+            </div>
+          </div>
+          </div>
+          <div id="text">
+          <h1>Bienvenue chez notre Auto Ecole. </h1><br><br>
+         <p> Le permis est une étape importante de notre vie. Notre auto-ecole s’engage à vous accompagner avec professionnalisme tout au long de votre formation au permis de conduire. Votre auto école vous accueille dans des locaux modernes . Nous vous accueillons au sein d’une équipe compétente et sympathique pour construire avec vous une relation de confiance.</div> 
+         </p>
+      </div>
+
+
+      <div id="formations">
+        <h1>Découvrez nos formules :</h1>
+        
+
+        <div id="animation">
+      
+          <div id="forma1">
+          <a href="formation.php">
+              <img src="imag/moto.png" alt="">
+              <span>
+                Formule A 
+                <br>
+                3 300 DH
+              </span>
+              </a> 
+          </div>
+        
+
+          <div id="forma2"> 
+          <a href="formation.php">
+              <img src="imag/voiture2.png" alt="">
+              <span>
+                Formule B 
+                <br>
+                3 550 DH
+              </span>
+              </a>   
+          </div>
+
+
+          <div id="forma3"> 
+            <a href="formation.php">
+              <img src="imag/camion.png" alt="">
+              <span>
+                Formule C 
+                <br>
+                3 800 DH
+              </span>
+            </a> 
+          </div>
+
+
+          <div id="forma4"> 
+            <a href="formation.php">
+              <img src="imag/bus1.png" alt="">
+              <span>
+                Formule D 
+                <br>
+                4 050 DH
+              </span>
+            </a> 
+          </div>
+
+        </div>
+
+        <div id="plus">
+
+        </div>
+      </div>
+      
+    <div id="pointFort">
+        <div id="title">
+          <p id="h2">Nos points forts :</p><br>
+          <span>-Suivi des Cours.</span><br><br>
+          <span>-Cours et tests de code animés par un formateur.</span><br><br>
+          <span>-Enseignement sur les dernières gammes de voiture.</span><br><br>
+          <span>-Equipe enseignante dynamique et à l’écoute de vos besoins.</span><br><br>
+          <span>-Grande disponibilité de planning pour les heures de conduite.</span><br><br>
+          <span>-Délais minimum pour les présentations aux examens.</span><br><br>
+        </div>
+
+    </div>
+
+      <div id="infos">
+        <div id="infos-center1">
+              <h1>Des formations adaptées aux besoins de chacun.</h1>
+              <p>Découvrez le contenu de nos offres et choisissez celle qui vous convient le mieux. Pour plus d'informations véuillez nous contacter.</p>
+              <a href="infos.php"><input type="button" value="Infos"> </a>
+              <a href="contact.php"><input type="button" value="Contact"></a>
+          <div id="infos-center2">
+          </div>
+        </div>  
+      </div>
+
+      <div id="choisir">
+          <h1>Pourquoi nous choisir ?</h1>
+          <div id="items">
+              <div id="arg1">
+                  <br>
+                  <h4>Voitures performantes:</h4><br>
+                  <p>Des voitures neuves et performantes.</p>
+              </div>
+
+              <div id="arg2">
+                <br>
+                  <h4>Apprenez le code en agence :</h4><br>
+                  <p>Suivez votre formation via le logiciel ou lors des cours théoriques.</p>
+              </div>
+
+              <div id="arg3">
+                <br>
+                  <h4>Pourcentage de réussite élevé :</h4><br>
+                  <p>Plus de 90% de nos étudiant réussi l'éxamen depuis la première tentative.</p>
+              </div>
+          </div>
+      </div>
+
+
+
+      <div id="copyright">
+
+      </div>
+
+
+
+
+  </div>
+ 
+
+</body>
+
+<footer>
+    
+</footer>
+</html>
